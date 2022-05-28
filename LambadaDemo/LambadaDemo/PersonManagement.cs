6a286f2c717af82385bb01062b3b7df798b7c256
snehasonwane <snehasonwane01@gmail.com>
2022-05-28 17:42:39 +0530

    [Manasi]Add : Adding Person Data

---
+++ LambadaDemo/LambadaDemo/PersonManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambadaDemo
+{
+    public class PersonManagement
+    {
+        List<Person> list = new List<Person>();
+        public void AddPerson()
+        {
+            list.Add(new Person { SSN = 101, name = "Ajay", Address = "Nashik", Age = 55 });
+            list.Add(new Person { SSN = 102, name = "Vijay", Address = "Delhi", Age = 55 });
+            list.Add(new Person { SSN = 103, name = "Aman", Address = "Lucknow", Age = 55 });
+            list.Add(new Person { SSN = 104, name = "Anish", Address = "Pune", Age = 55 });
+            list.Add(new Person { SSN = 105, name = "Suraj", Address = "Hydrabaad", Age = 55 });
+            list.Add(new Person { SSN = 106, name = "Anurag", Address = "Mumbai", Age = 55 });
+        }
+    }
+}
